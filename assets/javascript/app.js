5eab311b927dc08e8ea5b00e11ec8d2269233b34
Chelsea <TheGoldess@The-Goldess-2.local>
2017-09-26 23:05:35 -0500

    edits

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+function countdown(minutes) {
+    var seconds = 60;
+    var mins = minutes
+    function tick() {
+        //This script expects an element with an ID = "counter". You can change that to what ever you want. 
+        var counter = document.getElementById("timer");
+        var current_minutes = mins-1
+        seconds--;
+        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+            if(mins > 1){
+                countdown(mins-1);           
+            }
+        }
+    }
+    tick();
+}
