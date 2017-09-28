99eecaa290262d52b5745baa775ab05cea4bffb2
Chelsea <TheGoldess@The-Goldess-2.local>
2017-09-28 02:31:15 -0500

    editing shit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,24 @@
-//Setting up the countdown timer
+$( document ).ready(function(){
 
+$("#mainbox").hide();
+
+$("zbeginning").click(function(){
+
+$("#begin").hide();
+  $("#mainbox").show();
+  startTime();
+
+})
+
+$("#done").click(function(){
+  returnScore();
+})
+
+//Setting up the countdown timer
+function startTime(duration, display) {
 var count = 90;
 
+function startTime(duration, display){
 var gameTime = setInterval("counter()", 1000);
 
 function convertSeconds(s){
@@ -20,8 +37,54 @@ if (count === 0) {
 }
 }
 
+}
+
+jQuery(function ($) {
+    var fiveMinutes = 60 * 1.5,
+        display = $('#timer');
+    startTimer(fiveMinutes, display);
+});
+
+
+var userInput = new Array();
+var answers = new Array(5);
+
+answers[1]='c' //question 1 solution
+answers[2]='b' //question 2 solution, and so on.
+answers[3]='a'
+answers[4]='d'
+answers[5]='a'
+
+
+tot = answers.length;
+
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName( radioName ); // Get radio group by-name
+    for(var y=1; y<radios.length; y++)
+      if(radios[y].checked) return radios[y].value; // return the checked value
+}
+
+function getScore(){
+  var score = 0;
+  for (var i=0; i<tot; i++)
+    if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
+  return score;
+}
+
+function returnScore(){
+  $("#answers").show();
+  $("#box1").hide();
+
+  $("#result").text("Your score is "+ getScore() +" out of "+ tot +".");
+
+}
 
 
 
+
+
+
+
+});
 
 
